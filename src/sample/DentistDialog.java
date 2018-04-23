0f39794f9158e3416e574b01c0859493c609df80
Tokarz <kuba.tokarzewski@gmail.com>
2018-04-24 01:51:07 +0200

    DentistDialog secured v1.0 . Added comments in more confusing places.

--- src/sample/DentistDialog.java
+++ src/sample/DentistDialog.java
@@ -42,7 +42,9 @@ public class DentistDialog extends Dialog {
         TextField phone = new TextField();
         name.setPromptText("Pole obowiązkowe");
         surname.setPromptText("Pole obowiązkowe");
-        salary.setPromptText("Pole obowiązkowe");
+        salary.setPromptText("Pole obowiązkowe, max 9 cyfr");
+
+        //these 3 listeners will check if OK button can be pressed
 
         salary.textProperty().addListener((observable, oldValue, newValue) -> {
             if(thatLongIfStatement(name,surname,salary))
@@ -63,6 +65,9 @@ public class DentistDialog extends Dialog {
                 getDialogPane().lookupButton(ButtonType.OK).setDisable(false);
         });
 
+        //creating datePicker and disabling manually adding date with keyboard plus
+        //disabling choosing a date from the future
+
         DatePicker datePicker = new DatePicker();
         datePicker.setEditable(false);
         datePicker.setDayCellFactory(picker -> new DateCell(){
@@ -72,6 +77,9 @@ public class DentistDialog extends Dialog {
                 setDisable(empty || date.compareTo(today) > 0 );
             }
         });
+
+        // preparing Converter for chosen date
+        // for Database purposes I want the Date to be in format "yyyy-dd-MM"
 
         datePicker.setConverter(new StringConverter<LocalDate>() {
             String pattern = "yyyy-dd-MM";
@@ -111,8 +119,13 @@ public class DentistDialog extends Dialog {
         gridPane.add(datePicker,1,3);
         gridPane.add(phone, 1, 4);
 
+        //adding 2 buttons ( ok , cancel ) and disabling button ok
+
         this.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
         this.getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+
+        //preparing converter for a result returned by Dialog
+        //I want Dialog to return a brand new Dentist-class object
 
         setResultConverter(button->{
             if(button == ButtonType.OK) {
@@ -127,24 +140,31 @@ public class DentistDialog extends Dialog {
         this.getDialogPane().setContent(gridPane);
     }
 
+    //function used to check whether OK button can be clicked in createAddDialog() or not
+
     private boolean thatLongIfStatement(TextField name, TextField surname, TextField salary){
         int Salary;
-        if(!salary.getText().matches("[0-9]+$")) return true;
+        if(!salary.getText().matches("[0-9]+$") || !name.getText().matches("[a-zA-Z]+$") || !surname.getText().matches("[a-zA-z]+$") || salary.getText().length() >= 10) return true;
         else Salary = Integer.parseInt(salary.getText().toString());
-        return Salary <= 0 || Salary >= 130 || salary.getText().equals("") || name.getText().equals("") || surname.getText().equals("");
+        return Salary < 0 || salary.getText().equals("") || name.getText().equals("") || surname.getText().equals("");
     }
 
     private void createDeleteDialog(ArrayList list) {
         this.setTitle("Usuwanie dentysty");
         this.setHeaderText("Wybierz ID dentysty, którego chcesz usunąć");
         ComboBox choiceBox = new ComboBox();
+
+        //this is the list of Dentists' ids. These ids may be found in a table
         ObservableList<String> ids = FXCollections.observableArrayList(list);
         choiceBox.setItems(ids);
+
         gridPane.add(new Label("Wybierz ID dentysty:"),0,0);
         gridPane.add(choiceBox,1,0);
+
+        //if there is no ID chosen then return null, otherwise return chosen ID;
         setResultConverter(button->{
             if(button == ButtonType.OK) {
-
+                if(choiceBox.valueProperty().getValue() == null) return null;
                 return Integer.parseInt(choiceBox.getValue().toString());
             }
             else return null;
@@ -157,6 +177,7 @@ public class DentistDialog extends Dialog {
         this.setTitle("Edytowanie dentysty");
         this.setHeaderText("Wybierz ID dentysty, którego chcesz edytować");
         ComboBox choiceBox = new ComboBox();
+        //this is the list of Dentists' ids. These ids may be found in a table
         ObservableList<String> ids = FXCollections.observableArrayList(list);
         choiceBox.setItems(ids);
         ArrayList<TextField> textFields = new ArrayList<>();
@@ -164,19 +185,44 @@ public class DentistDialog extends Dialog {
         TextField surname = new TextField();
         TextField salary = new TextField();
         TextField phone = new TextField();
+
+        //these 3 listeners will check if OK button can be pressed
+        salary.textProperty().addListener((observable, oldValue, newValue) -> {
+            if((salary.getText().length() >0 && !salary.getText().matches("[0-9]+$")) || (name.getText().length() > 0 && !name.getText().matches("[a-zA-Z]+$")) || ( surname.getText().length() > 0 && !surname.getText().matches("[a-zA-z]+$")) || salary.getText().length() >= 10)
+                getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+            else
+                getDialogPane().lookupButton(ButtonType.OK).setDisable(false);
+        });
+        name.textProperty().addListener((observable, oldValue, newValue) -> {
+            if((salary.getText().length() >0 && !salary.getText().matches("[0-9]+$")) || (name.getText().length() > 0 && !name.getText().matches("[a-zA-Z]+$")) || ( surname.getText().length() > 0 && !surname.getText().matches("[a-zA-z]+$")) || salary.getText().length() >= 10)
+                getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+            else
+                getDialogPane().lookupButton(ButtonType.OK).setDisable(false);
+        });
+        surname.textProperty().addListener((observable, oldValue, newValue) -> {
+            if((salary.getText().length() >0 && !salary.getText().matches("[0-9]+$")) || (name.getText().length() > 0 && !name.getText().matches("[a-zA-Z]+$")) || ( surname.getText().length() > 0 && !surname.getText().matches("[a-zA-z]+$")) || salary.getText().length() >= 10)
+                getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+            else
+                getDialogPane().lookupButton(ButtonType.OK).setDisable(false);
+        });
+
+        //adding my textfields to an ArrayList textFields to have easier work when operating on all textfields
         textFields.add(name);
         textFields.add(surname);
         textFields.add(salary);
         textFields.add(phone);
+
         for(TextField textField : textFields){
             textField.setDisable(true);
             textField.setPromptText("Pole nieobowiązkowe");
         }
+        //if there is no id chosen, then user cannot add changes;
         choiceBox.valueProperty().addListener((observable, oldValue, newValue) -> {
             for(TextField textField : textFields){
                 textField.setDisable(false);
             }
         });
+        //adding all things to gridPane
         gridPane.add(new Label("Wybierz ID:"),0,0);
         gridPane.add(choiceBox,1,0);
         gridPane.add(new Label("Zmień imię:"),0,1);
@@ -188,6 +234,12 @@ public class DentistDialog extends Dialog {
         gridPane.add(salary,1,3);
         gridPane.add(phone,1,4);
 
+        //preparing converter for a result returned by Dialog
+        //I want Dialog to return a brand new Dentist-class object
+        //if there is no salary edited, then I set it on -1,
+        //because later in DBManager I check its value and if it's -1
+        //then I apply no changes to the column salary in table
+
         setResultConverter(button->{
             if(button == ButtonType.OK) {
                 int SALARY;
@@ -198,6 +250,7 @@ public class DentistDialog extends Dialog {
             else return null;
         });
         this.getDialogPane().getButtonTypes().addAll(ButtonType.OK,ButtonType.CANCEL);
+        this.getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
         this.getDialogPane().setContent(gridPane);
     }
 }
